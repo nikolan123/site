ac22025d1b1b3ac350224cd84604021564cabc89
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2026-01-17 13:03:34 +0200

    add game list

---
+++ games.js
@@ -0,0 +1,170 @@
+const games = {
+  "portal": {
+    "name": "Portal",
+    "store": "https://store.steampowered.com/app/400/Portal/",
+    "steamdb": "https://steamdb.info/app/400/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/400/c7cb09b9f0fbb9589b4bd5a8217c8333c4d8204e.ico"
+  },
+  "portal 2": {
+    "name": "Portal 2",
+    "store": "https://store.steampowered.com/app/620/Portal_2/",
+    "steamdb": "https://steamdb.info/app/620/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/620/25a5a16b2423bf7487ac5340b5b0948cef48c5f8.ico"
+  },
+  "dishonored": {
+    "name": "Dishonored",
+    "store": "https://store.steampowered.com/app/205100/Dishonored/",
+    "steamdb": "https://steamdb.info/app/205100/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/205100/bfef4bd645bed431efdb5fe701b1d2b2a754ae21.ico"
+  },
+  "dishonored 2": {
+    "name": "Dishonored 2",
+    "store": "https://store.steampowered.com/app/403640/Dishonored_2/",
+    "steamdb": "https://steamdb.info/app/403640/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/403640/a7353bffe65192a4a87008380d49c0e2ff9327ca.ico"
+  },
+  "dishonored: doto": {
+    "name": "Dishonored: DoTO",
+    "store": "https://store.steampowered.com/app/614570/Dishonored_Death_of_the_Outsider/",
+    "steamdb": "https://steamdb.info/app/614570/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/614570/3de04d3c750210b3db500e85950663cc69be3eb4.ico"
+  },
+  "infra": {
+    "name": "INFRA",
+    "store": "https://store.steampowered.com/app/251110/INFRA/",
+    "steamdb": "https://steamdb.info/app/251110/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/251110/78c622c09ce3cc3220b8d6ea3d0a109151bf1777.ico"
+  },
+  "dying light": {
+    "name": "Dying Light",
+    "store": "https://store.steampowered.com/app/239140/Dying_Light/",
+    "steamdb": "https://steamdb.info/app/239140/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/239140/8f48620805a37de0206fac8897f66022675a5789.ico"
+  },
+  "dying light 2": {
+    "name": "Dying Light 2",
+    "store": "https://store.steampowered.com/app/534380/Dying_Light_2_Stay_Human_Reloaded_Edition/",
+    "steamdb": "https://steamdb.info/app/534380/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/534380/b1355acf75d0e7b605ed286a1a43a46602583d98.ico"
+  },
+  "dying light: the beast": {
+    "name": "Dying Light: The Beast",
+    "store": "https://store.steampowered.com/app/3008130/Dying_Light_The_Beast/",
+    "steamdb": "https://steamdb.info/app/3008130/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/3008130/48a57f9344332b2c9c16c3590a14af7b39abd8eb.ico"
+  },
+  "firewatch": {
+    "name": "Firewatch",
+    "store": "https://store.steampowered.com/app/383870/Firewatch/",
+    "steamdb": "https://steamdb.info/app/383870/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/383870/ca54ab65a8e3df43f58d08e2628228764b307dc0.ico"
+  },
+  "the stanley parable": {
+    "name": "The Stanley Parable",
+    "store": "https://store.steampowered.com/app/1703340/The_Stanley_Parable_Ultra_Deluxe/",
+    "steamdb": "https://steamdb.info/app/1703340/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/1703340/a59e0688c3e380396ba3d33f52e454cf9cd6b6aa.ico"
+  },
+  "the entropy centre": {
+    "name": "The Entropy Centre",
+    "store": "https://store.steampowered.com/app/1730590/The_Entropy_Centre/",
+    "steamdb": "https://steamdb.info/app/1730590/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/1730590/6352595b976e53bf5136ce7f49c2940e23d33c8c.ico"
+  },
+  "euro truck simulator 2": {
+    "name": "Euro Truck Simulator 2",
+    "store": "https://store.steampowered.com/app/227300/Euro_Truck_Simulator_2/",
+    "steamdb": "https://steamdb.info/app/227300/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/227300/5daee883eed0789df7a96d8ffe89def35edc1431.ico"
+  },
+  "the talos principle": {
+    "name": "The Talos Principle",
+    "store": "https://store.steampowered.com/app/257510/The_Talos_Principle/",
+    "steamdb": "https://steamdb.info/app/257510/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/257510/0518809d7fc702807d7b3ee0b7f4bc351be7bfae.ico"
+  },
+  "left 4 dead": {
+    "name": "Left 4 Dead",
+    "store": "https://store.steampowered.com/app/500/Left_4_Dead/",
+    "steamdb": "https://steamdb.info/app/500/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/500/dea36504f7c4dac7c657e77a0fac8a274e2af2d3.ico"
+  },
+  "left 4 dead 2": {
+    "name": "Left 4 Dead 2",
+    "store": "https://store.steampowered.com/app/550/Left_4_Dead_2/",
+    "steamdb": "https://steamdb.info/app/550/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/550/1a8d50f6078b5d023582ea1793b0e53813d57b7f.ico"
+  },
+  "evacuation": {
+    "name": "Evacuation",
+    "store": "https://store.steampowered.com/app/2224920/Evacuation/",
+    "steamdb": "https://steamdb.info/app/2224920/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/2224920/284e43f55e6529a18ae78ecd7d143071e28d0d78.ico"
+  },
+  "estranged": {
+    "name": "Estranged",
+    "store": "https://store.steampowered.com/app/261820/Estranged_Act_I/",
+    "steamdb": "https://steamdb.info/app/261820/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/261820/280363b23ba68671595a1f0db6c9c1e51a84faa2.ico"
+  },
+  "road 96": {
+    "name": "Road 96",
+    "store": "https://store.steampowered.com/app/1466640/Road_96/",
+    "steamdb": "https://steamdb.info/app/1466640/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/1466640/582a40bc63e13894561c233f0f06a7996962d880.ico"
+  },
+  "a way out": {
+    "name": "A Way Out",
+    "store": "https://store.steampowered.com/app/1222700/A_Way_Out/",
+    "steamdb": "https://steamdb.info/app/1222700/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/1222700/fe57a746a93adb703666fbf457be97abf5e187ab.ico"
+  },
+  "undertale": {
+    "name": "UNDERTALE",
+    "store": "https://store.steampowered.com/app/391540/Undertale/",
+    "steamdb": "https://steamdb.info/app/391540/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/391540/60e1002598c4aacebe1e0818964117d1d8da13af.ico"
+  },
+  "celeste": {
+    "name": "Celeste",
+    "store": "https://store.steampowered.com/app/504230/Celeste/",
+    "steamdb": "https://steamdb.info/app/504230/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/504230/fbcf1fbb408b277b7fa0e601a3b87d82b6b2a531.ico"
+  },
+  "the vanishing of ethan carter": {
+    "name": "The Vanishing of Ethan Carter",
+    "store": "https://store.steampowered.com/app/258520/The_Vanishing_of_Ethan_Carter/",
+    "steamdb": "https://steamdb.info/app/258520/info/",
+    "icon": "https://shared.fastly.steamstatic.com/community_assets/images/apps/258520/d2e02ce83cbcb621e3c6349fd9ecc68c1ef8550d.ico"
+  }
+};
+
+function shuffleArray(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
+function initializeGames() {
+    const gamesArray = Object.values(games);
+    const shuffledGames = shuffleArray(gamesArray);
+    
+    const gamesList = document.getElementById('games-list');
+    if (!gamesList) return;
+    
+    shuffledGames.forEach(game => {
+        const gameLink = document.createElement('a');
+        gameLink.className = 'game';
+        gameLink.href = game.store;
+        gameLink.innerHTML = `
+            <img src="${game.icon}" width="16" alt="${game.name}">
+            <p>${game.name}</p>
+        `;
+        gamesList.appendChild(gameLink);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initializeGames);
